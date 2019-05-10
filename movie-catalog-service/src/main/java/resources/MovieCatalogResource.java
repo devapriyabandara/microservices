453e446f01043ada795362f0e857982139727934
devapriyabandara <devapriya.bandara@zealsolutions.com.au>
2019-05-10 15:53:47 +1000

    Adding movie catalog resources and catalog items

---
+++ movie-catalog-service/src/main/java/resources/MovieCatalogResource.java
@@ -0,0 +1,22 @@
+package resources;
+
+import com.microservicestest.moviecatalogservice.models.CatalogItems;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItems> getCatalog(@PathVariable("userId") String userId){
+
+        return Collections.singletonList(
+            new CatalogItems("Minions", "Movie Name", 4)
+        );
+    }
+}
